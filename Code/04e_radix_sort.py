a1f8147177337d8c7560a5c622c7a93b350fe6d6
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2021-05-30 18:18:03 -0300

    Adaptçao do Radix Sort para strings de 4 digitos

--- Code/04e_radix_sort.py
+++ Code/04e_radix_sort.py
@@ -1,24 +1,25 @@
-def couting_sort(A, indice, k):
+def couting_sort_str(A, indice, k):
     n = len(A)
     ordenado = [''] * n
     quantidade = [0] * k
 
     for j in range(n): 
-        quantidade[int(A[j][indice])] += 1
+        quantidade[ord(A[j][indice])] += 1
     for i in range(1, k): 
         quantidade[i] += quantidade[i-1]
     for j in reversed(range(0, n)):
-        ordenado[quantidade[int(A[j][indice])]-1] = A[j]
-        quantidade[int(A[j][indice])] -= 1
+        ordenado[quantidade[ord(A[j][indice])]-1] = A[j]
+        quantidade[ord(A[j][indice])] -= 1
     return ordenado
 
 def radix_sort(A, n_digitos):
+    n_unicode = 256
     for i in reversed(range(n_digitos)):
-        A = couting_sort(A, i, 10)
+        A = couting_sort_str(A, i, n_unicode)
         print(A)
     return 0
 
 if __name__ == "__main__":
-    A = ['12','20','56','13','18','15','30','31','20','49']
-    n_digitos = 2
+    A = ['abcd', 'bcda', 'abcd', 'poxa', 'poze', 'vida', 'rodo', 'xypc']
+    n_digitos = 4
     radix_sort(A, n_digitos)
