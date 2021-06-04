566109b46820f01b0a33e5a5c22cb11076385a77
Vinicius Matias <45612047+matiasvinicius@users.noreply.github.com>
2021-06-04 20:17:55 -0300

    Adição da lista ligada dinâmica

--- Code/05d_lista_ligada_dinamica.py
+++ Code/05d_lista_ligada_dinamica.py
@@ -23,7 +23,7 @@ class Lista:
             endereco = endereco.prox
         return tamanho
     
-    def tamanho(self):
+    def imprimir_lista(self):
         endereco = self.inicio
         print("Lista de Registros")
         while endereco != None:
@@ -48,10 +48,55 @@ class Lista:
             atual = atual.prox
         return None, None
 
-    def inserir(self, chave):
+    def inserir(self, reg):
+        chave = reg.chave
         anterior, atual = self.busca_dupla(chave)
-        if atual == 
+        if atual != None: return False
+        if anterior == None:
+            novo = Elemento(reg, l.inicio)
+            self.inicio = novo
+        else:
+            novo = Elemento(reg, anterior.prox)
+            anterior.prox = novo        
+        return True
 
+    def excluir(self, chave):
+        anterior, atual = self.busca_dupla(chave)
+        if atual == None: return False
+        if anterior == None: 
+            self.inicio = atual.prox
+        else: 
+            anterior.prox = atual.prox
+        return True
+
+    #Não é possível limpar explicitamente a memória 
+    #de objetos em Python, então devemos apenas
+    #perder a referência à eles e o garbage collector
+    #irá removê-los em algum momento
+    def reinicializar_lista(self):
+        self.inicio = None
+        return 0
 
 if __name__ == "__main__":
     l = Lista()
+    reg1 = Registro(1, 'b8')
+    reg2 = Registro(2, 'a3')
+    reg3 = Registro(3, 'a5')
+    reg4 = Registro(4, 'b1')
+
+    l = Lista()
+    l.inicializar_lista()
+    l.inserir(reg2)
+    l.inserir(reg1)
+    l.inserir(reg3)
+    l.inserir(reg4)
+    l.inserir(reg4)
+    l.imprimir_lista()
+    l.excluir('b7')
+    l.excluir('b8')
+    l.imprimir_lista()
+    l.reinicializar_lista()
+    l.imprimir_lista()
+    l.inserir(reg2)
+    l.imprimir_lista()
+
