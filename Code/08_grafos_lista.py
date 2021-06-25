6339cbc6f30b277a3e4feb24cfdf23eb03e72df9
Vinicius <45612047+matiasvinicius@users.noreply.github.com>
2021-06-25 16:52:04 -0300

    Algoritmo de Dijkstra para grafos

--- Code/08_grafos_lista.py
+++ Code/08_grafos_lista.py
@@ -1,3 +1,4 @@
+from math import dist, inf
 from queue import Queue
 
 # Grafo como Lista de Adjacências
@@ -53,7 +54,7 @@ class Grafo:
         
         while v != None:
             if cor[v.vertice] == 0:
-                achou = g.visita_profundidade(v.vertice, cor, info)
+                achou = self.visita_profundidade(v.vertice, cor, info)
                 if not(achou is False): return achou
             v = v.prox
         
@@ -64,7 +65,7 @@ class Grafo:
         cor = [0 for _ in range(self.vertices)]
         for u in range(self.vertices):
             if cor[u] == 0:
-                achou = g.visita_profundidade(u, cor, info)
+                achou = self.visita_profundidade(u, cor, info)
                 if not(achou is False): return achou
                 print("----")
         return False
@@ -76,15 +77,15 @@ class Grafo:
 
         while not(f.empty()):
             u = f.get()
-            v = g.arr_vertices[u].cabeca
-            print('Visita nó', g.arr_vertices[u].info)
+            v = self.arr_vertices[u].cabeca
+            print('Visita nó', self.arr_vertices[u].info)
             
             if self.arr_vertices[u].info == info:
                 print('Achou nó', info)
                 return True
             
             explorados[u] = True
-            while v:
+            while v != None:
                 if explorados[v.vertice] is False:
                     explorados[v.vertice] = True
                     f.put(v.vertice)
@@ -97,11 +98,53 @@ class Grafo:
 
         for u in range(self.vertices):
             if explorados[u] == False:
-                achou = g.visita_largura(explorados, u, info)
+                achou = self.visita_largura(explorados, u, info)
                 if achou is True: return achou
             print("----")
         
         return False
+
+    def relaxa(self, dist, pre, u, v):
+        ad = self.arr_vertices[u].cabeca
+        while ad != None and ad.vertice != v:
+            ad = ad.prox
+        if ad != None:
+            if dist[v] > dist[u] + ad.peso:
+                dist[v] = dist[u] + ad.peso
+                pre[v] = u
+
+    def existe_aberto(self, aberto):
+        for u in aberto:
+            if u is True: return True
+        return False
+
+    def menor_dist(self,aberto, dist):
+        i = 0
+        while i < self.vertices:
+            if aberto[i]: break
+            i += 1
+        if i == self.vertices: return None
+        menor = i
+        for i in range(menor+1, self.vertices):
+            if aberto[i] and dist[menor] > dist[i]:
+                menor = i
+        return menor
+
+    def dijkstra(self, v_ini):
+        distancia = [inf for i in range(self.vertices)]
+        predecessor = [None for i in range(self.vertices)]
+        aberto = [True for i in range(self.vertices)]
+        distancia[v_ini] = 0
+
+        while self.existe_aberto(aberto):
+            u = self.menor_dist(aberto, distancia)
+            aberto[u] = False
+            ad = self.arr_vertices[u].cabeca
+            while ad: 
+                self.relaxa(distancia, predecessor, u, ad.vertice)
+                ad = ad.prox
+
+        return distancia
 
 if __name__ == "__main__":
     g = Grafo(10)
@@ -128,3 +171,25 @@ if __name__ == "__main__":
     print()
     print(g.busca_largura('L'))
     print()
+
+    num_vertices = 5
+    g2 = Grafo(num_vertices)
+    for i in range(num_vertices):
+        g2.set_info_vertice(i, chr(65+i))
+    
+    g2.criar_aresta(0,1,1)
+    g2.criar_aresta(0,2,2)
+    g2.criar_aresta(1,2,2)
+    g2.criar_aresta(1,4,3)
+    g2.criar_aresta(2,3,1)
+    g2.criar_aresta(2,4,5)
+    g2.criar_aresta(3,4,4)
+    g2.criar_aresta(3,1,2) 
+    g2.criar_aresta(3,3,0)
+    g2.criar_aresta(4,2,2)
+
+    g2.imprimir()
+    print("Dijkstra - Nó 0 do Grafo 1:", g.dijkstra(0))    
+    print("Dijkstra - Nó 2 do Grafo 1:", g.dijkstra(2))    
+    print("Dijkstra - Nó 0 do Grafo 2:", g2.dijkstra(0))    
+    print("Dijkstra - Nó 2 do Grafo 2:", g2.dijkstra(2))
